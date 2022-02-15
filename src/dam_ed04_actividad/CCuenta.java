67b9cb1e1f1490ed4e3e6ff1b43e40581f70d71a
Carlos Guzman <gloderina90@gmail.com>
2022-02-15 22:22:03 +0100

    Version_Inicial

--- src/dam_ed04_actividad/CCuenta.java
+++ src/dam_ed04_actividad/CCuenta.java
@@ -5,7 +5,10 @@
 
 package dam_ed04_actividad;
 
-
+/**
+ *
+ * @author CARLOS
+ */
 public class CCuenta {
 
     /**
@@ -42,11 +45,20 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
-
+    /**
+     *
+     */
     public CCuenta()
     {
     }
 
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -54,23 +66,38 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     *
+     * @param nom
+     */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
 
+    /**
+     *
+     * @return
+     */
     public String obtenerNombre()
     {
         return getNombre();
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double estado()
     {
         return getSaldo();
     }
 
-
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -78,6 +105,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -87,28 +119,43 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
-
+    /**
+     *
+     * @return
+     */
     public String obtenerCuenta()
     {
         return getCuenta();
     }
 
-
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     *
+     * @param tipoInterés
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
